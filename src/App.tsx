f8ab0d4bc6c7ca4abd0318715239164688258bb8
l.conti <l.conti@namirial.it>
2023-08-29 17:51:27 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  return (
+    <>
+      <Spline scene="https://prod.spline.design/RFnpAMpOUq8vNKxH/scene.splinecode" />
+    </>
+  )
+}
+
+export default App
