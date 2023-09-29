ae091c346c1d7b0acb2a99c902a9a704453463e0
simone <simone.recupero@intre.it>
2023-09-29 14:26:50 +0200

    Added new pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ import Spline from '@splinetool/react-spline';
 function App() {
   return (
     <>
-      <Spline scene="https://prod.spline.design/RFnpAMpOUq8vNKxH/scene.splinecode" />
+      <div className="section">
+        <Spline scene="https://prod.spline.design/YViuqisJHgS1Wy4v/scene.splinecode" />
+        <h1 className="landing-title">NINTENDO<br/>SWITCH</h1>
+      </div>
+      <div className="section">
+        <div className="col">
+          <Spline scene="https://prod.spline.design/27p4AXgvHV5LD3Sw/scene.splinecode" />
+        </div>
+        <div className="col">
+          <h2>Titolo wow</h2>
+          <p className="fixed-dim">Zò desfà pestulàda marcantòne fiocà formài. Saltà uzà spenelà braghe zùen cantinér.</p>
+          <button>Compra</button>
+        </div>
+      </div>
     </>
   )
 }
