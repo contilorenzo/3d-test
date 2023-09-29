1fd94a35f565b9c2f0328482d100928679f14783
ace <roberto.aceti@intre.it>
2023-09-29 15:32:59 +0200

    FIx html & css

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,35 @@ import './App.css'
 import Spline from '@splinetool/react-spline';
 
 function App() {
-  return (
-    <>
-      <div className="section">
-        <Spline scene="https://prod.spline.design/YViuqisJHgS1Wy4v/scene.splinecode" />
-        <h1 className="landing-title">NINTENDO<br/>SWITCH</h1>
-      </div>
-      <div className="section">
-        <div className="col">
-          <Spline scene="https://prod.spline.design/27p4AXgvHV5LD3Sw/scene.splinecode" />
-        </div>
-        <div className="col">
-          <h2>Titolo wow</h2>
-          <p className="fixed-dim">Zò desfà pestulàda marcantòne fiocà formài. Saltà uzà spenelà braghe zùen cantinér.</p>
-          <button>Compra</button>
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="section section--landing">
+                <img className={"logo"} src={"/nintendoLogo.png"} alt="Ninendo Logo" height="92"></img>
+                <Spline scene="https://prod.spline.design/elv65hIGsU4k2hEY/scene.splinecode" />
+                <h1 className="landing-title">NINTENDO<br/>SWITCH</h1>
+            </div>
+            <div className="section">
+                <div className="col">
+                    <h2>Titolo wow</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur. Libero nunc et ultrices nulla elit neque ullamcorper ultricies.</p>
+                    <button>Scopri di più</button>
+                </div>
+                <div className="col">
+                    <Spline scene="https://prod.spline.design/27p4AXgvHV5LD3Sw/scene.splinecode"/>
+                </div>
+            </div>
+            <div className="section">
+                <div className="col">
+                    <Spline scene="https://prod.spline.design/27p4AXgvHV5LD3Sw/scene.splinecode"/>
+                </div>
+                <div className="col">
+                    <h2>Titolo wow</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur. Libero nunc et ultrices nulla elit neque ullamcorper ultricies.</p>
+                    <button>Compra</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
